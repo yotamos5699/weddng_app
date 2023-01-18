8b33d9ca9475be63a927ff4b1ad5dbd4902e1438
yotamos5699 <m.lhafakot@gmail.com>
2023-01-18 12:53:30 +0200

    NEW COMMIT

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,7 +42,7 @@ const Home: NextPage = () => {
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
+              href="/Info"
               target="_blank"
             >
               <h3 className="text-2xl font-bold">עלינו →</h3>
@@ -52,7 +52,7 @@ const Home: NextPage = () => {
             </Link>
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
+              href="Recommendation"
               target="_blank"
             >
               <h3 className="text-2xl font-bold">לקוחות ממליצים →</h3>
